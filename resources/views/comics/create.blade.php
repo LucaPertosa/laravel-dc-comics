4fd3b2e48da6d0fbd76cd4633f0e62b2f3b4b751
LucaPertosa <luca.pertosa@hotmail.it>
2023-05-29 18:18:02 +0200

    Add: function store in ComicController to add comic data in db

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h2 class="text-center">Aggiungi un nuovo fumetto alla lista</h2>
 
-        <form action="" method="POST">
+        <form action="{{ route('comics.store') }}" method="POST">
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
@@ -27,17 +27,17 @@
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="text" class="form-control" id="price" name="cooking_time">
+                <input type="text" class="form-control" id="price" name="price">
             </div>
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data di uscita</label>
-                <input type="text" class="form-control" id="sale_date" name="weight">
+                <input type="text" class="form-control" id="sale_date" name="sale_date">
             </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control" id="series" name="weight">
+                <input type="text" class="form-control" id="series" name="series">
             </div>
 
             <div class="mb-3">
@@ -45,7 +45,7 @@
                 <textarea class="form-control" name="description" id="description" rows="3"></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="submit" class="btn btn-success">Invia</button>
 
         </form>
     </div>
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="container">
         <h2 class="text-center">Tutti i nostri fumetti</h2>
-        <div class="row text-end">
-            <a href="{{ route('comics.create') }}">
-                <i class="fa-solid fa-plus"></i>Inserisci un nuovo fumetto 
+        <div class="my-3 text-end">
+            <a href="{{ route('comics.create') }}" class="btn btn-success">
+                <i class="fa-solid fa-plus"></i> Inserisci un nuovo fumetto 
             </a>
         </div>
         <table class="table">
